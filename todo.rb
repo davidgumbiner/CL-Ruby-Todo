2d73224b8342b0664e27325c1585c20d7d40a980
David Gumbiner <dagumbiner@gmail.com>
2012-07-13 15:47:41 -0700

    Object-Oriented, checks for existing CSV and creates new if absent

--- todo.rb
+++ todo.rb
@@ -3,6 +3,75 @@ require "csv"
 #tlist.csv = CSV file
 #tlist = writing to CSV file
 #current_list = array version of CSV
+#current_session = main object of Todo_App class
+
+class Todo_App
+  
+  def print_list
+    #print new file
+    if File.exist?("tlist.csv")
+      current_list = CSV.read("tlist.csv")
+      current_list.each_with_index {|row, index| puts "#{index+1}. #{row[0]}"}
+    else
+      puts "Try adding something to the list first!"
+  end
+  end
+  
+  def overwrite(updated_list)
+    #overwrite file with new array
+    tlist = CSV.open('tlist.csv', 'w') 
+    updated_list.each {|row| tlist << row}
+    tlist.close
+  end
+  
+  def all_list
+    #view all incomplete todos
+    print_list
+  end
+  
+  def new_item
+    #create a new todo item
+    
+    #create an array and add the new item
+    
+    if File.exist?("tlist.csv")
+    next_item = ARGV[1]
+    current_list = CSV.read("tlist.csv")
+    current_list << [next_item]
+    
+    overwrite(current_list)
+    print_list
+    
+  else
+    
+    tlist = CSV.open('tlist.csv', 'w') do |csv|
+    csv << ""
+    tlist.close
+  end
+    
+    next_item = ARGV[1]
+    current_list = CSV.read("tlist.csv")
+    current_list << [next_item]
+    
+    overwrite(current_list)
+    print_list
+  end
+  end
+  
+  def complete
+    complete_index = (ARGV[1].to_i - 1)
+
+    #create an array and delete row
+    current_list = CSV.read("tlist.csv")
+    current_list.delete_at(complete_index)
+
+    overwrite(current_list)
+    print_list
+  end
+  
+end
+
+current_session = Todo_App.new
 
   #print instructions
   if ARGV[0] == nil
@@ -15,43 +84,17 @@ require "csv"
 
   #view all incomplete todos
   if ARGV[0] == "all"
-  current_list = CSV.read("tlist.csv")
-  current_list.each_with_index {|row, index| puts "#{index+1}. #{row}"}
+    current_session.all_list
   end
 
 
   #create a new todo item
   if ARGV[0] == "new"
-  
-  #create an array and add the new item
-  current_list = CSV.read("tlist.csv")
-  next_item = ARGV[1]
-  current_list << [next_item]
-  
-  #overwrite file with new array
-  tlist = CSV.open('tlist.csv', 'w') 
-  current_list.each {|row| tlist << row}
-  tlist.close
-  
-  #print new file
-  current_list.each_with_index {|row, index| puts "#{index+1}. #{row}"}
+    current_session.new_item
   end
 
 
   #mark the todo item as completed
   if ARGV[0] == "complete"
-  
-  complete_index = (ARGV[1].to_i - 1)
-  
-  #create an array and delete row
-  current_list = CSV.read("tlist.csv")
-  current_list.delete_at(complete_index)
-  
-  #overwrite file with new array
-  tlist = CSV.open('tlist.csv', 'w') 
-  current_list.each {|row| tlist << row}
-  tlist.close
-  
-  #print new file
-  current_list.each_with_index {|row, index| puts "#{index+1}. #{row}"}
+    current_session.complete
   end
